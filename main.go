3a871ab878bebebac64d56a07f3749312e486338
朱亮 <teamide@163.com>
2023-11-26 17:40:12 +0800

    提交测试

--- main.go
+++ main.go
@@ -1,16 +1,31 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/shirou/gopsutil/v3/host"
+	"os"
 	"runtime"
 	"time"
 )
 
 func main() {
 
+	var err error
+	hostname, err := os.Hostname()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("hostname:", hostname)
 	fmt.Println("GOOS:", runtime.GOOS)
 	fmt.Println("GOARCH:", runtime.GOARCH)
-	var err error
+
+	info, err := host.Info()
+	if err != nil {
+		panic(err)
+	}
+	bs, _ := json.Marshal(info)
+	fmt.Println("info:", string(bs))
 	terminal := &Terminal{}
 	err = terminal.Start()
 	if err != nil {
